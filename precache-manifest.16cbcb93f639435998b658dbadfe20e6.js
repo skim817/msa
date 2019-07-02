cb6bcf649d0f9dfcc555ba6bb8deee054c77277d
skim817 <skim817@aucklanduni.ac.nz>
2019-07-03 01:11:39 +1200

    Updates

---
+++ precache-manifest.16cbcb93f639435998b658dbadfe20e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d50ea3f4c7929e5beb3e1ad2616550a5",
+    "url": "skim817.github.io/fist%20website/index.html"
+  },
+  {
+    "revision": "5e84d3a6c7822327e7e7",
+    "url": "skim817.github.io/fist%20website/static/css/main.8c5089d2.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "skim817.github.io/fist%20website/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "5e84d3a6c7822327e7e7",
+    "url": "skim817.github.io/fist%20website/static/js/main.0d8aeee1.chunk.js"
+  },
+  {
+    "revision": "17f3d86ae8d251b0588c",
+    "url": "skim817.github.io/fist%20website/static/js/runtime~main.afd16680.js"
+  }
+]);
